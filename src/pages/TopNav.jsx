d9f49eb08d43b3ab6c812029ae1584e82a97fc3a
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2020-04-15 21:20:11 -0400

    deploy

--- src/pages/TopNav.jsx
+++ src/pages/TopNav.jsx
@@ -7,7 +7,11 @@ function TopNav() {
     <div className="nav">
       <div className="topNav">
         <div>
-          <a className="navBtn" href="/contact" target="_blank">
+          <a
+            className="navBtn"
+            href="https://serene-keller-f79a3c.netlify.app/"
+            target="_blank"
+          >
             CONTACT
           </a>
           <a className="navBtn" href="html_images.asp">
